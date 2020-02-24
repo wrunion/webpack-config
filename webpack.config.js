68a2e7840c718b8292d954da34a5f5e542953338
Winter Runion <winterrunion@gmail.com>
2020-02-23 22:59:46 -0800

    Add CleanWebpack and UglifyJs plugins.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+//ask about the uglify plugin security issue i'm getting from npm
 
 module.exports = {
   entry: './src/main.js',
@@ -8,6 +12,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new UglifyJsPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Ping Pong',
       template: './src/index.html',
